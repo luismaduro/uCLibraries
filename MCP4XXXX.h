5c4ca6b2283717abb30c6e40ff787c9db9dd96a4
Luis Maduro <luismaduro88@gmail.com>
2012-09-25 01:15:13 +0100

    Added new library to interface with the MCP4XXXX
    Library to interface with the MCP4XXXX digital potenciometers from
    Microchip

---
+++ MCP4XXXX.h
@@ -0,0 +1,37 @@
+/**
+ *  @file      MCP4XXXX.h
+ *  @brief     Library to interface with the MCP4XXXX digital potenciometers.
+ *  @author    Luis Maduro
+ *  @version   1.0
+ *  @date      September 2012
+ */
+
+#ifndef _MCP41XXXX_H_
+#define _MCP41XXXX_H_
+
+#define MCP4XXXX_WRITE_DATA_POT_0           0b00010001
+#define MCP4XXXX_WRITE_DATA_POT_1           0b00010010
+#define MCP4XXXX_WRITE_DATA_BOTH_POTS       0b00010011
+
+#define MCP4XXXX_SHUTDOWN_POT_0             0b00100001
+#define MCP4XXXX_SHUTDOWN_POT_1             0b00100010
+#define MCP4XXXX_SHUTDOWN_BOTH_POTS         0b00100011
+
+#endif /* _SPIDEV_H_ */
+
+
+void MCP4XXXXShutdownPotenciometer0(volatile unsigned char *port,
+                                    unsigned char portBit);
+void MCP4XXXXShutdownPotenciometer1(volatile unsigned char *port,
+                                    unsigned char portBit);
+void MCP4XXXXShutdownBothPotenciometers(volatile unsigned char *port,
+                                        unsigned char portBit);
+void MCP4XXXXSetPotenciometer0Value(volatile unsigned char *port,
+                                    unsigned char portBit,
+                                    unsigned char resistance);
+void MCP4XXXXSetPotenciometer1Value(volatile unsigned char *port,
+                                    unsigned char portBit,
+                                    unsigned char resistance);
+void MCP4XXXXSetPotenciometersValue(volatile unsigned char *port,
+                                    unsigned char portBit,
+                                    unsigned char resistance);
