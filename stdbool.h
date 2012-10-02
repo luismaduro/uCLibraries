256921132ab81fe018a2db062de89ba5c1542ed0
Luis Maduro <luismaduro88@gmail.com>
2012-10-02 18:48:25 +0100

    Bug fix
    Renamed typedef that causes conflicts in the XC8 compiler. Variable now
    is bolean instead of bool.

--- stdbool.h
+++ stdbool.h
@@ -9,7 +9,7 @@
 #ifndef __bool_true_and_false_are_defined
 #define __bool_true_and_false_are_defined
 
-typedef unsigned char bool;
+typedef unsigned char boolean;
 
 #define true 1
 #define false 0
