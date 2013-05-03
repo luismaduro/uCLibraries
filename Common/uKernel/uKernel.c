d0489c63ff1b3d20ab713ed733ade102e592d972
Luis Maduro <luismaduro88@gmail.com>
2013-05-03 23:59:37 +0100

    Another clean up

--- Common/uKernel/uKernel.c
+++ Common/uKernel/uKernel.c
@@ -5,26 +5,13 @@
  *  @date           03/05/2013
  *  @copyright		GNU General Public License
  *
- *  @brief Scheduler for microcontrollers. 
- *  This projects was written in C programming language and it
- *  is not an RTOS with a lot functions to manage registers,
- *  priorities and so forth for microcontroller. But this
- *  project is like a cooperative "RTOS" focusing on a tiny
- *  tasks scheduling for ALL microcontrollers. You need to
- *  configure just one timer (e.g. 1 millisecond, is a good
- *  choice) to get a tiny tasks scheduler ready. Your only limit
- *  is the microcontroller memory. This kernel can create a task
- *  with a period, stop, resume and change a period of a task at
- *  any time. This kernel can delete all task at any time to
- *  create another sequencer as you want. A TickGet function is
- *  supplied by the kernel to manage all timer as you want.
- *  For use this source code in microcontroller you have to
- *  create just one timer interrupt function and increment the
- *  variable usTickCount to get your tiny-kernel for your
- *  own application.
- *  It is willingly written in a general way to help people to
- *  customise for theirs owns applications. There are no
- *  priority between task like a round-robin task scheduling.
+ *  @brief This is a scheduler for micrcontrollers. 
+ *  This is not any kind of RTOS, or anything like it. 
+ *  Just create a function, create a descriptor for that function and added to 
+ *  the scheduler with a period and let the scheduler do the rest. There is no 
+ *	priority and I am tring to keep it really simple due to the memory 
+ *  limitations of micrcontrollers. The maximum number of task is 255 but I am
+ *  sure that the memory will go out first. If anyone needs more tasks let me know.
  */
  
 #include "uKernel.h"
